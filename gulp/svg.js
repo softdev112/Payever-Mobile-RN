c3cb5f445ec79c4158323ac4706db03e3b8d3687
Alexey Prokhorov <megahertz@l96.ru>
2016-12-20 21:29:45 +0500

    Add gulp task for converting svg icons to icon font

---
+++ gulp/svg.js
@@ -0,0 +1,143 @@
+'use strict';
+
+const fs       = require('fs');
+const path     = require('path');
+const https    = require('https');
+const Readable = require('stream').Readable;
+
+const cheerio = require('cheerio');
+const gulp    = require('gulp-task-doc');
+const File    = require('vinyl');
+
+const svgicons2svgfont = require('gulp-svgicons2svgfont');
+const svg2ttf          = require('gulp-svg2ttf');
+
+const PAGE_URL = 'https://payeverworldwide.github.io/';
+const SVG_TEMPLATE = `
+  <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  <svg 
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="{width}"
+    height="{height}"
+  >
+    {symbol}
+    <use xlink:href="#{id}" />
+  </svg>
+`;
+
+let glyphs = [];
+
+/**
+ * Download svg icons from payever ui-kit and transform to icon font
+ */
+gulp.task('svg', ['svg:convert'], () => {
+  const glyphDictionary = {};
+  glyphs.forEach((glyph) => {
+    glyphDictionary[glyph.name] = {
+      unicode: glyph.unicode[0],
+      color: glyph.color
+    }
+  });
+
+  const json = JSON.stringify(glyphDictionary, null, '  ');
+  const filePath = path.join(
+    __dirname, '..', 'src', 'common', 'ui', 'Icon', 'map.json'
+  );
+  fs.writeFileSync(filePath, json);
+});
+
+// @internal
+gulp.task('svg:convert', () => {
+  return makeSvgStream()
+    .pipe(svgicons2svgfont({
+      fontName: 'payeverIcons',
+      normalize: true
+    }))
+    .on('glyphs', g => glyphs = g)
+    .pipe(svg2ttf())
+    .pipe(gulp.dest('android/app/src/main/assets/fonts'));
+});
+
+/**
+ * Read svg from payever ui-kit and return Readable Stream
+ * @return {Promise<Readable>}
+ */
+function makeSvgStream() {
+  const stream = new Readable({ objectMode: true });
+  const symbolsPromise = getPage(PAGE_URL)
+    .then(extractSymbols)
+    .then(wrapSvg);
+
+  let processing = false;
+
+  stream._read = function() {
+    if (processing) {
+      return;
+    }
+
+    symbolsPromise.then((symbols) => {
+      symbols.forEach((symbol) => {
+        stream.push(new File({
+          cwd:      __dirname,
+          base:     __dirname,
+          path:     path.join(__dirname, symbol.id + '.svg'),
+          contents: new Buffer(symbol.html)
+        }));
+      });
+      stream.push(null);
+    }).catch((e) => {
+      process.nextTick(() => stream.emit('error', e));
+    });
+
+    processing = true;
+  };
+
+  return stream;
+}
+
+function getPage(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+      res
+        .on('data', d => data += d)
+        .on('end', () => resolve(data));
+    })
+      .on('error', reject)
+      .end();
+  });
+}
+
+function extractSymbols(html) {
+  return new Promise((resolve) => {
+    const $ = cheerio.load(html);
+
+    const $symbols = $('svg[data-id^=icons] symbol')
+      .map((i, element) => {
+        const $symbol = $(element);
+        const [,,width, height] = $symbol.attr('viewbox').split(' ');
+        return {
+          width,
+          height,
+          id: $symbol.attr('id'),
+          html: $.html($symbol)
+        };
+      });
+
+    resolve($symbols.get());
+  });
+}
+
+function wrapSvg(symbols) {
+  return new Promise((resolve) => {
+    symbols.forEach((symbol) => {
+      symbol.html = SVG_TEMPLATE
+        .replace('{width}', symbol.width)
+        .replace('{height}', symbol.height)
+        .replace('{symbol}', symbol.html)
+        .replace('{id}', symbol.id);
+    });
+    resolve(symbols);
+  });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const { spawn, spawnSync } = require('child_process');
 const gulp  = require('gulp-task-doc');
-
 
 gulp.task('help', gulp.help());
 gulp.task('default', ['help']);
 
-require('./gulp/main');
 require('./gulp/android');
 require('./gulp/ios');
+require('./gulp/main');
+require('./gulp/svg');
