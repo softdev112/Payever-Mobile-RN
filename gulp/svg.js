c1e12a8387b258bbadb1d25a0ef0caee97f48539
Alexey Prokhorov <megahertz@l96.ru>
2016-12-21 20:49:06 +0500

    Update generated icon font location for ios

--- gulp/svg.js
+++ gulp/svg.js
@@ -57,7 +57,7 @@ gulp.task('svg:convert', () => {
     .on('glyphs', g => glyphs = g)
     .pipe(svg2ttf())
     .pipe(gulp.dest('android/app/src/main/assets/fonts'))
-    .pipe(gulp.dest('ios/PayeverMobile/Resources/Fonts'));
+    .pipe(gulp.dest('ios/PayeverMobile/Assets/Fonts'));
 });
 
 /**
