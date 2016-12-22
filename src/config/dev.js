ecd96acd5f76e2745e0c2d83b86bdf9be2404885
Alexey Prokhorov <megahertz@l96.ru>
2016-12-22 20:25:42 +0500

    Delete unnecessary comment

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,9 +1,5 @@
 export default {
   siteUrl: 'https://mein.payever.de',
-  // It's better to prevent using clientSecret. But after analyzing a lot
-  // of resources I've made a decision that is's impossible. FOSOAuthServerBundle
-  // doesn't support password auth without client_secret
-  // https://github.com/FriendsOfSymfony/FOSOAuthServerBundle/issues/115
   api: {
     baseUrl: 'https://mein.payever.de',
     clientId: '1633_3vi6g4uiwmyow8044o4w0wo4s88cogosw84kw888kw408wok8c',
