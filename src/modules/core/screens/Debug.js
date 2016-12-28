d3fd8889c1ac14453bb5c4b3251fec488b875fb3
Alexey Prokhorov <megahertz@l96.ru>
2016-12-28 22:31:08 +0500

    Experiment: Icons on debug page

--- src/modules/core/screens/Debug.js
+++ src/modules/core/screens/Debug.js
@@ -1,71 +1,54 @@
 /* eslint-disable */
 
 import { Component } from 'react';
-import { inject, observer } from 'mobx-react/native';
-import { action } from 'mobx';
-import { ListView } from 'react-native';
-import { Button, StyleSheet, Text, View } from 'ui';
-import SearchStore, { SearchRow }  from '../../../store/SearchStore';
+import { Image } from 'react-native';
+import { Icon, StyleSheet, Text, View } from 'ui';
 
-@inject('search')
-@observer
+
+const EARLY_LOADING = require('../../../store/UserProfilesStore/images/no-business.png');
+
 export default class Debug extends Component {
   props: {
     navigator: Navigator;
-    search: SearchStore;
   };
 
-  onSearchClick() {
-    action('Search', () => {
-      this.props.search.search(Math.random().toString(36).substr(2, 1));
-    })();
-  }
-
-  onRowClick(row: SearchRow) {
-    if (row.is_following) {
-      this.props.search.unfollow(row.id)
-    } else {
-      this.props.search.follow(row.id)
-    }
-  }
-
-  renderRow(row: SearchRow) {
-    return (
-      <Text onPress={() => this.onRowClick(row)}>
-        {row.name} {row.is_following ? '1' : '0'}
-      </Text>
-    );
-  }
-
   render() {
-    const { search } = this.props;
-
-    log('Render', search.items);
-
-    const dataSource = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    const itemsDs = dataSource.cloneWithRows(search.items.slice());
-
     return (
-      <View style={styles.container}>
-        <Text>
-          {this.props.search.isFollowUnfollowUpdating ? 'updating' : ''}
-        </Text>
-
-        <Button title="Search" onPress={::this.onSearchClick} />
-        <ListView
-          dataSource={itemsDs}
-          renderRow={::this.renderRow}
-        />
+      <View>
+        <View style={styles.row}>
+          <Icon name="icon-search-16" />
+          <Text>icon-search-16 Icon</Text>
+        </View>
+        <View style={styles.row}>
+          <Image
+            style={styles.image}
+            source={require('../../../common/ui/Icon/images/icon-search-16.png')}
+          />
+          <Text>icon-search-16 Direct</Text>
+        </View>
+        <View style={styles.row}>
+          <Image
+            style={styles.image}
+            source={require('../../../store/UserProfilesStore/images/no-business.png')}
+            />
+          <Text>no-business1</Text>
+        </View>
+        <View style={styles.row}>
+          <Image style={styles.image} source={EARLY_LOADING} />
+          <Text>no-business early loading</Text>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-
+  row: {
+    flexDirection: 'row'
   },
+
+  image: {
+    width: 16,
+    height: 16,
+  }
 });
