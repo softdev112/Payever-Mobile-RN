60c7030d0ad0d05faebe876d0caa7166bd0fca6d
elf2707 <jelf2707@gmail.com>
2016-12-29 12:25:02 +0300

    Search

--- src/modules/dashboard/screens/Search.js
+++ src/modules/dashboard/screens/Search.js
@@ -127,7 +127,7 @@ export default class SearchForm extends Component {
 
         {!query && (
           <TouchableWithoutFeedback onPress={::this.onClose}>
-          <View style={{ flex: 1 }}/>
+            <View style={{ flex: 1 }} />
           </TouchableWithoutFeedback>
         )}
       </View>
