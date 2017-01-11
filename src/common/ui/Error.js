88b9a756c40314da8a9768b4b47641c6d18aacd0
softdev112 <mappdevm@gmail.com>
2017-01-11 11:51:09 +0300

    Changed cursor color in Android. Rebuilt login functionality to be more user friendly

--- src/common/ui/Error.js
+++ src/common/ui/Error.js
@@ -3,14 +3,50 @@ import { Text } from 'react-native';
 import StyleSheet from './StyleSheet';
 
 export default class Error extends Component {
+  static defaultProps = { duration: 0 };
+
   props: {
     message: ?string;
     style?: Object | number;
+    duration?: number; // ms
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: true,
+    };
+  }
+
+  componentDidMount() {
+    // Start timer if duration is set
+    this.setUpShowTimer();
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({ show: true });
+    this.setUpShowTimer();
+  }
+
+  componentWillUnmount() {
+    if (this.showTimer) clearTimeout(this.showTimer);
+  }
+
+  setUpShowTimer() {
+    const { duration } = this.props;
+
+    if (duration === 0 || this.showTimer) return;
+
+    this.showTimer = setTimeout(() => {
+      this.setState({ show: false });
+      this.showTimer = null;
+    }, duration);
+  }
 
   render() {
     const { message } = this.props;
-    if (!message) {
+    if (!message || !this.state.show) {
       return null;
     }
 
--- src/common/ui/TextInput.js
+++ src/common/ui/TextInput.js
@@ -1,8 +1,19 @@
 import { Component } from 'react';
+import { Keyboard } from 'react-native';
 import TextField from 'react-native-md-textinput';
+
 
 export default class TextInput extends Component {
   $input: TextField;
+
+  shouldComponentUpdate(nexProps, nextState) {
+    if(!this.$input) return false;
+
+    // To prevent label fall down when value === ''
+    // because of error in the TextField Component
+    return nexProps.value !== ''
+      && (!!this.props.securityTextEntry && this.$input.isFocused());
+  }
 
   focus() {
     this.$input.focus();
