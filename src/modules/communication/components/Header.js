126e20da0214fd36b05ffbd6325ba2a4abe7d1fc
Alexey Prokhorov <megahertz@l96.ru>
2017-01-27 09:03:30 +0500

    Communication: Group header

--- src/modules/communication/components/Header.js
+++ src/modules/communication/components/Header.js
@@ -1,4 +1,4 @@
-import { Text } from 'ui';
+import { Icon, StyleSheet, Text, View } from 'ui';
 
 const TITLES = {
   direct: 'DIRECT MESSAGES',
@@ -7,9 +7,35 @@ const TITLES = {
 
 export default function Header({ type }: PropTypes) {
   return (
-    <Text>{TITLES[type]}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{TITLES[type]}</Text>
+      <Icon
+        style={styles.add}
+        source="icon-plus-circle-24"
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    margin: 10,
+  },
+
+  title: {
+    color: '#959ba3',
+    flex: 1,
+    fontSize: 10,
+  },
+
+  add: {
+    color: '$pe_icons_color',
+    fontSize: 16,
+    width: 16,
+    height: 17,
+  },
+});
 
 type PropTypes = {
   type: string;
