f7979ab989623b49088fce0feae5597a9e0a84ab
Alexey Prokhorov <megahertz@l96.ru>
2017-02-02 08:46:14 +0500

    Config: Allow to override dev setting locally

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,8 +1,13 @@
-export default {
+import local from './local';
+
+export default Object.assign({
   siteUrl: 'https://mein.payever.de',
   api: {
     baseUrl: 'https://mein.payever.de',
     clientId: '1633_3vi6g4uiwmyow8044o4w0wo4s88cogosw84kw888kw408wok8c',
     clientSecret: '63nqx0kppzocw4sswok8gg800sk4w4kko4k0oo8gs4ow00wgos',
   },
-};
+  debug: {
+    logApiCall: true,
+  },
+}, local);
