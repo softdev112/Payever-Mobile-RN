5836e3420361e958619f812fb45ca6f6e84c13af
elf2707 <jelf2707@gmail.com>
2017-02-22 19:05:12 +0300

    MD-65 Set RNN NavBar - off. Set Android deep links path to /selfterminal/new

--- src/modules/pos/screens/Terminal.js
+++ src/modules/pos/screens/Terminal.js
@@ -3,6 +3,10 @@ import { WebView } from 'ui';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class Terminal extends Component {
+  static navigatorStyle = {
+    navBarHidden: true,
+  };
+
   props: {
     url: string;
   };
