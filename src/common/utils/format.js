82659856fda3b053f8dd2400a224df30c391eb43
Alexey Prokhorov <megahertz@l96.ru>
2017-02-22 18:03:45 +0500

    Close MD-45 Communication: Display attachment files

--- src/common/utils/format.js
+++ src/common/utils/format.js
@@ -1,12 +1,22 @@
 import moment from 'moment';
 
 export default {
-  currency(num, currency = '€') {
+  currency(num, currency = '€'): string {
     num = parseFloat(num) || 0;
     return num.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + currency;
   },
 
-  timeFromNow(date) {
+  timeFromNow(date): string {
     return moment(date).fromNow();
+  },
+
+  /**
+   * Return human readable size with metric prefix
+   */
+  size(bytes: number): string {
+    if (bytes === 0) return '0 B';
+    const e = Math.floor(Math.log(bytes) / Math.log(1000));
+    return +(bytes / (1000 ** e)).toFixed(1) + ' ' +
+      'BKMGTP'.charAt(e).replace('B', '') + 'B';
   },
 };
