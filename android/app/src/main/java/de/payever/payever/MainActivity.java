d5711f804043546909be97bfbe2e459472c23bdd
elf2707 <jelf2707@gmail.com>
2017-03-02 15:47:07 +0300

    MD-80 Add some refactoring

--- android/app/src/main/java/de/payever/payever/MainActivity.java
+++ android/app/src/main/java/de/payever/payever/MainActivity.java
@@ -1,9 +1,6 @@
 package de.payever.payever;
 
 import com.reactnativenavigation.controllers.SplashActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends SplashActivity {
 }
