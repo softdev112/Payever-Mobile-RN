7543b16024720b5bd89b9a534b75945983fcdff9
softdev112 <mappdevm@gmail.com>
2017-03-28 19:14:45 +0300

    MD-98 Start working on it Add contacts to chat by sending direct messages

--- src/modules/communication/screens/AddContact.js
+++ src/modules/communication/screens/AddContact.js
@@ -1,29 +1,53 @@
 import { Component } from 'react';
 import { inject, observer } from 'mobx-react/native';
-import { NavBar, StyleSheet, View } from 'ui';
+import { FlatTextInput, NavBar, StyleSheet, View } from 'ui';
 import AddContactBlock from '../components/contacts/AddContactBlock';
-import ProfilesStore from '../../../store/profiles';
 
-@inject('profiles')
+@inject('communication')
 @observer
 export default class AddContact extends Component {
   static navigatorStyle = {
     navBarHidden: true,
   };
 
-  props: {
-    profiles?: ProfilesStore;
+  $messageTextInput: FlatTextInput;
+
+  state: {
+    message: string;
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: '',
+    };
+  }
+
+  onAddContacts() {
+    // communication.sendMsgToSelectedContacts(this.state.message);
+  }
+
   render() {
-    this.props.profiles.loadAllContacts();
     return (
       <View style={styles.container}>
         <NavBar popup>
           <NavBar.Back />
           <NavBar.Title title="Add a contact" />
+          <NavBar.Button title="Send" onPress={::this.onAddContacts} />
         </NavBar>
-        <AddContactBlock />
+        <View style={styles.content}>
+          <FlatTextInput
+            ref={ref => this.$messageTextInput = ref}
+            placeholder="Message"
+            onChangeText={text => this.setState({ message: text })}
+            value={this.state.message}
+          />
+          <AddContactBlock
+            style={styles.addContactBlock}
+            bottomDockStyle={styles.bottomDockStyle}
+          />
+        </View>
       </View>
     );
   }
@@ -32,5 +56,19 @@ export default class AddContact extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  content: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+
+  addContactBlock: {
+    marginTop: 10,
+  },
+
+  bottomDockStyle: {
+    $topHeight: '64%',
+    top: '$topHeight',
   },
 });
