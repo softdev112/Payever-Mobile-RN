26005cf259a432beb84bae8360dc8571d7fca250
softdev112 <mappdevm@gmail.com>
2017-03-28 17:02:47 +0300

    bump

--- src/modules/communication/screens/AddContact.js
+++ src/modules/communication/screens/AddContact.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { inject, observer } from 'mobx-react/native';
-import { NavBar, StyleSheet, View, WebView } from 'ui';
+import { NavBar, StyleSheet, View } from 'ui';
+import AddContactBlock from '../components/contacts/AddContactBlock';
 import ProfilesStore from '../../../store/profiles';
 
 @inject('profiles')
@@ -15,51 +16,16 @@ export default class AddContact extends Component {
   };
 
   render() {
-    const profile = this.props.profiles.currentProfile;
-
-    const uri = profile.getCommunicationUrl() + '#new/message';
-    const js = `(${injectedJs.toString()})()`;
-
+    this.props.profiles.loadAllContacts();
     return (
       <View style={styles.container}>
         <NavBar popup>
           <NavBar.Back />
           <NavBar.Title title="Add a contact" />
         </NavBar>
-        <WebView
-          injectJs={js}
-          showLoader
-          showNavBar="never"
-          source={{ uri }}
-        />
+        <AddContactBlock />
       </View>
     );
-  }
-}
-
-// eslint-disable-next-line consistent-return
-function injectedJs() {
-  /* eslint-disable */
-  /** @name callWebViewOnMessage */
-  /** @name Messenger */
-  /** @name Messenger.Application */
-  /** @name Messenger.View */
-  /** @name Messenger.View.ModalFormView */
-
-  Messenger.View.ModalFormView.prototype.childEvents = function() {
-    setTimeout(showDialog, 300);
-  };
-
-  Messenger.Application.prototype.closeModal = function() {
-    callWebViewOnMessage({ command: 'back' });
-  };
-
-  function showDialog() {
-    callWebViewOnMessage({ command: 'hide-loader' });
-    var $btnCancel = document.querySelector('.btn-default');
-    if ($btnCancel) {
-      $btnCancel.style.display = 'none';
-    }
   }
 }
 
